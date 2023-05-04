4addc3a35ffc23b88bb22fbfa89d066a69db7456
kevinchege47 <chegekevin001@gmail.com>
2023-05-04 18:37:49 +0300

    first commit

---
+++ src/main/java/com/kevin47/Spring/security/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.kevin47.Spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kevin47/Spring/security/controller/RegistrationController.java
@@ -0,0 +1,7 @@
+package com.kevin47.Spring.security.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+}
